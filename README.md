e3baa167fc9d3d2dad4baaaa67929a41d2c33aab
Sunset Mikoto <26019675+SunsetMkt@users.noreply.github.com>
2025-03-10 22:01:13 +0800

    chore: init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # HarmonyOS_Sans_SC_Webfont_Splitted
-HarmonyOS Sans SC Webfont Splitted by KonghaYao/cn-font-split
+
+HarmonyOS Sans SC Webfont Splitted by [KonghaYao/cn-font-split](https://github.com/KonghaYao/cn-font-split/tree/release/packages/ffi-js).
+
+## Get Fonts
+
+[Docs](https://developer.huawei.com/consumer/cn/doc/design-guides/font-0000001828772001#section55510217189) or [Download Zip](https://developer.huawei.com/images/download/next/HarmonyOS-Sans.zip)
+
+## Quick Start
+
+```sh
+pnpm i cn-font-split
+node split.js
+```
---
+++ split.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+import { fontSplit } from "cn-font-split";
+
+async function split(input, outDir) {
+    const inputBuffer = new Uint8Array(fs.readFileSync(input).buffer);
+
+    console.log(`Splitting ${input}...`);
+
+    console.time("node");
+    await fontSplit({
+        input: inputBuffer,
+        outDir: outDir,
+    });
+    console.timeEnd("node");
+}
+
+// For every .ttf in HarmonyOS_Sans_SC folder
+for (const file of fs.readdirSync("./HarmonyOS_Sans_SC")) {
+    if (file.endsWith(".ttf")) {
+        split(
+            `./HarmonyOS_Sans_SC/${file}`,
+            `./HarmonyOS_Sans_SC_Webfont_Splitted/${file}`
+        );
+    }
+}
